c00c10bd5436bc83f98d1c2005468f20f0a2917f
merinav <81377750+merinav@users.noreply.github.com>
2022-03-04 15:05:53 +0200

    Text Change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - GIT - new branch
+          Learn React (NEW FEATURE BRANCH)
         </a>
       </header>
     </div>
